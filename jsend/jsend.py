7d931aee1b8104081032a8b19960685f140a3061
Sangwon Lee <gamzabaw@gmail.com>
2015-07-16 22:20:31 +0900

    replace tab with spaces, removed .idea

--- jsend/jsend.py
+++ jsend/jsend.py
@@ -4,47 +4,47 @@ __author__ = 'zirony'
 
 
 class DictEx(dict):
-	def jsonify(self):
-		return json.dumps(self)
+    def jsonify(self):
+        return json.dumps(self)
 
 
 def success(data={}):
-	if not isinstance(data, dict):
-		raise ValueError('data must be the dict type')
-	return DictEx({'status': 'success', 'data': data})
+    if not isinstance(data, dict):
+        raise ValueError('data must be the dict type')
+    return DictEx({'status': 'success', 'data': data})
 
 
 def fail(data={}):
-	if not isinstance(data, dict):
-		raise ValueError('data must be the dict type')
-	return DictEx({'status': 'fail', 'data': data})
+    if not isinstance(data, dict):
+        raise ValueError('data must be the dict type')
+    return DictEx({'status': 'fail', 'data': data})
 
 
 def error(message='', code=None, data=None):
-	ret = {}
-	if not isinstance(message, str):
-		raise ValueError('message must be the str type')
-	if code:
-		if not isinstance(code, int):
-			raise ValueError('code must be the int type')
-		ret['code'] = code
-	if data:
-		if not isinstance(data, dict):
-			raise ValueError('data must be the dict type')
-		ret['data'] = data
+    ret = {}
+    if not isinstance(message, str):
+        raise ValueError('message must be the str type')
+    if code:
+        if not isinstance(code, int):
+            raise ValueError('code must be the int type')
+        ret['code'] = code
+    if data:
+        if not isinstance(data, dict):
+            raise ValueError('data must be the dict type')
+        ret['data'] = data
 
-	ret['status'] = 'error'
-	ret['message'] = message
-	return DictEx(ret)
+    ret['status'] = 'error'
+    ret['message'] = message
+    return DictEx(ret)
 
 
 def is_success(jsend_type):
-	return jsend_type['status'] == 'success'
+    return jsend_type['status'] == 'success'
 
 
 def is_fail(jsend_type):
-	return jsend_type['status'] == 'fail'
+    return jsend_type['status'] == 'fail'
 
 
 def is_error(jsend_type):
-	return jsend_type['status'] == 'error'
+    return jsend_type['status'] == 'error'
--- tests/test_tool_jsend.py
+++ tests/test_tool_jsend.py
@@ -1,72 +1,71 @@
-import json
 from unittest.case import TestCase
 
+import json
 import jsend
 
 
 class TestJsend(TestCase):
-	def test_success(self):
-		ret = jsend.success(data={'key': 'value'})
-		self.assertTrue(jsend.is_success(ret))
-		self.assertEqual(ret['data']['key'], 'value')
+    def test_success(self):
+        ret = jsend.success(data={'key': 'value'})
+        self.assertTrue(jsend.is_success(ret))
+        self.assertEqual(ret['data']['key'], 'value')
 
-	def test_success_data_must_be_dict(self):
-		try:
-			jsend.success(data=1)
-		except ValueError:
-			return
-		self.fail()
+    def test_success_data_must_be_dict(self):
+        try:
+            jsend.success(data=1)
+        except ValueError:
+            return
+        self.fail()
 
-	def test_fail(self):
-		ret = jsend.fail(data={'key': 'value'})
-		self.assertTrue(jsend.is_fail(ret))
-		self.assertEqual(ret['data']['key'], 'value')
+    def test_fail(self):
+        ret = jsend.fail(data={'key': 'value'})
+        self.assertTrue(jsend.is_fail(ret))
+        self.assertEqual(ret['data']['key'], 'value')
 
-	def test_fail_data_must_be_dict(self):
-		try:
-			jsend.fail(data=1)
-		except ValueError:
-			return
-		self.fail()
+    def test_fail_data_must_be_dict(self):
+        try:
+            jsend.fail(data=1)
+        except ValueError:
+            return
+        self.fail()
 
-	def test_error(self):
-		ret = jsend.error(message='error message')
-		self.assertTrue(jsend.is_error(ret))
-		self.assertEqual(ret['message'], 'error message')
+    def test_error(self):
+        ret = jsend.error(message='error message')
+        self.assertTrue(jsend.is_error(ret))
+        self.assertEqual(ret['message'], 'error message')
 
-	def test_error_message_must_be_str(self):
-		try:
-			jsend.error(message=1)
-		except ValueError:
-			return
-		self.fail()
+    def test_error_message_must_be_str(self):
+        try:
+            jsend.error(message=1)
+        except ValueError:
+            return
+        self.fail()
 
-	def test_error_code_must_be_number(self):
-		try:
-			jsend.error(code='1')
-		except ValueError:
-			return
-		self.fail()
+    def test_error_code_must_be_number(self):
+        try:
+            jsend.error(code='1')
+        except ValueError:
+            return
+        self.fail()
 
-	def test_error_data_must_be_dict(self):
-		try:
-			jsend.error(data=1)
-		except ValueError:
-			return
-		self.fail()
+    def test_error_data_must_be_dict(self):
+        try:
+            jsend.error(data=1)
+        except ValueError:
+            return
+        self.fail()
 
-	def test_jsend_to_jsonify(self):
-		ret_json_string = jsend.success().jsonify()
-		ret_json = json.loads(ret_json_string)
-		self.assertTrue(jsend.is_success(ret_json))
+    def test_jsend_to_jsonify(self):
+        ret_json_string = jsend.success().jsonify()
+        ret_json = json.loads(ret_json_string)
+        self.assertTrue(jsend.is_success(ret_json))
 
+    def test_jsend_to_jsonify_f(self):
+        ret_json_string = jsend.success().jsonify()
+        try:
+            jsend.is_success(ret_json_string)
+        except TypeError:
+            return
 
-	def test_jsend_to_jsonify_f(self):
-		ret_json_string = jsend.success().jsonify()
-		try:
-			jsend.is_success(ret_json_string)
-		except TypeError:
-			return
-
-		self.fail()
+        self.fail()
 
